832dae6c7a4ab7ab95d844ca435d6fddc6277118
Lrunner <viacheslav.dubrovskii.@mail.ru>
2020-10-10 14:34:33 +0300

    Added CinemaAdviser

---
+++ CinemaAdviser/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CinemaAdviser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, друг! Эта программа поможет тебе подобрать фильм на вечер!");
+            Console.WriteLine("Как тебя зовут?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, " + name);
+            Console.WriteLine("Фильм какого жанра вы бы хотели посмотреть? Комедия, драма или фантастика?");
+            string genre = Console.ReadLine();
+            if (genre == "комедия") {
+                Console.WriteLine("Pulp Fiction - yor choice");
+            } else if (genre == "драма") {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Sex Education - your choice");
+            } else if (genre == "фантастика") {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Люди икс норм так");
+            } else {
+                Console.WriteLine("You are sick");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
